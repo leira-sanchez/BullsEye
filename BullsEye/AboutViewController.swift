8219ef0b1b29530fc14577364bf8829db609165c
Leira C Sanchez Quinones <leira.sq@gmail.com>
2020-11-24 17:05:48 -0500

    added styilng and info view

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Leira C. Sanchez Quinones  on 11/24/20.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+  @IBAction func close() {
+    dismiss(animated: true, completion: nil)
+  }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
