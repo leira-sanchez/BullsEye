3ee1a8a5d38397ef9531069395a3bfa115ecd022
Leira C Sanchez Quinones <leira.sq@gmail.com>
2020-11-25 22:43:57 -0500

    done

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -6,11 +6,24 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
-  @IBAction func close() {
-    dismiss(animated: true, completion: nil)
-  }
+    @IBOutlet var webView: WKWebView!
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      if let url = Bundle.main.url(
+        forResource: "BullsEye", withExtension: "html") {
+        let request = URLRequest(url: url)
+        webView.load(request)
+      }
+    }
 
 
     /*
